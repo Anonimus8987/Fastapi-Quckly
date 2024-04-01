4ade0973e3fdfa2a58433ba890a1ff4b55209a6c
Anonimus8987 <abdulazizsobitov2004@gmail.com>
2024-04-01 23:13:36 +0500

    готово

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ import motor.motor_asyncio
 DOMAIN = os.getenv("DOMAIN")
 
 # MongoDB connection
-MONGODB_NAME = "quickly" # os.getenv("MONGODB_NAME")
-MONGODB_COL = "quickly" # os.getenv("MONGODB_COL")
+MONGODB_NAME = "quickly" 
+MONGODB_COL = "quickly" 
 MONGODB_URL = os.getenv("MONGODB_URL")
 mongoClient =  motor.motor_asyncio.AsyncIOMotorClient(MONGODB_URL)
 mongodb = mongoClient[MONGODB_NAME][MONGODB_COL]
-MONGODB_NAME2 = "quicklyAuth" # os.getenv("MONGODB_NAME2")
-MONGODB_COL2 = "quicklyAuth" # os.getenv("MONGODB_COL2")
+MONGODB_NAME2 = "quicklyAuth" 
+MONGODB_COL2 = "quicklyAuth" 
 MONGODB_URL2 = os.getenv("MONGODB_URL2")   
 mongoAuthClient =  motor.motor_asyncio.AsyncIOMotorClient(MONGODB_URL)
 authdb = mongoClient[MONGODB_NAME2][MONGODB_COL2]
